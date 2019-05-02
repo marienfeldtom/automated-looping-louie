74be70fac4a5f11e8e6bf2afabc8bfff73a1ecce
Tom Marienfeld <marienfeldtom@googlemail.com>
2019-05-02 16:14:35 +0200

    Update mechanics.py

--- mechanics.py
+++ mechanics.py
@@ -3,7 +3,7 @@ def makeDrink(player, drink):
     print (player.name + " bekommt einen " + drink.name)
     for ingredient in drink.ingredients:
         openVentil(ingredient.ventil, ingredient.time)
-    print ("Getränk wurde erfolgreich eingefuellt")
+    print ("Getraenk wurde erfolgreich eingefuellt")
 
 
 def openVentil(ventil, time):
