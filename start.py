b715aee7f1023cf6443695ca48c7e4af4f210b90
Tom Marienfeld <marienfeldtom@googlemail.com>
2019-05-02 16:42:10 +0200

    Update start.py

--- start.py
+++ start.py
@@ -13,11 +13,11 @@ for player in players:
     print(player.name)
 running = True
 
-GPIO.setup(2, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 print("An")
-GPIO.output(2, GPIO.HIGH)
+GPIO.output(21, GPIO.HIGH)
 time.sleep(3)
 print("Aus")
-GPIO.output(2, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
 print("Fertig")
 GPIO.cleanup()
