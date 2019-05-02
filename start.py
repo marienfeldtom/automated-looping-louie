072b6f37c96aa41d51048211bc6304d8074e8fe5
Tom Marienfeld <marienfeldtom@googlemail.com>
2019-05-02 16:00:17 +0200

    requirements

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ from classes import Drink
 from classes import Ingredient
 from mechanics import makeDrink
 from mechanics import openVentil
+import wiringpi
 
 players = [Player("Tom", 3), Player("Lasse", 3), Player("Jan", 3), Player("Jonas", 3)]
 print("Es spielen mit:")
@@ -10,4 +11,3 @@ for player in players:
     print(player.name)
 running = True
 while(running):
-    print("LÄUFT")
