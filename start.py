f92875ba83a8923800b4fc7479b125bd5bbab48c
Tom Marienfeld <marienfeldtom@googlemail.com>
2019-05-02 16:10:05 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,11 +3,19 @@ from classes import Drink
 from classes import Ingredient
 from mechanics import makeDrink
 from mechanics import openVentil
-import wiringpi
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
 players = [Player("Tom", 3), Player("Lasse", 3), Player("Jan", 3), Player("Jonas", 3)]
 print("Es spielen mit:")
 for player in players:
     print(player.name)
 running = True
-while(running):
+
+print("An")
+GPIO.output(2, GPIO.HIGH)
+time.sleep(3)
+print("Aus")
+GPIO.output(2, GPIO.LOW)
+print("Fertig")
